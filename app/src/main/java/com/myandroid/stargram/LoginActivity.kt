69810ae8395ac671990564cf5d2ae7958eb16214
KimRyeongHyeon <52704419+KimRyeongHyeon@users.noreply.github.com>
2020-05-27 11:41:04 +0900

    2강

--- app/src/main/java/com/myandroid/stargram/LoginActivity.kt
+++ app/src/main/java/com/myandroid/stargram/LoginActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +28,44 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess) {
+                var account = result!!.signInAccount
+                firebaseAuthAuthWithGoogle(account)
+            }
+        }
+    }
+
+    fun firebaseAuthAuthWithGoogle(account : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     fun signinAndSignup() {
@@ -27,7 +73,7 @@ class LoginActivity : AppCompatActivity() {
             ?.addOnCompleteListener {
                 task ->
                     if(task.isSuccessful) {
-                        moveMainPage(task.result!!.user)
+                        moveMainPage(task.result?.user)
                     } else if(!task.exception?.message.isNullOrEmpty()) {
                         Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                     } else {
@@ -40,7 +86,7 @@ class LoginActivity : AppCompatActivity() {
         auth?.signInWithEmailAndPassword(email_editText.text.toString(), password_editText.text.toString())
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    moveMainPage(task.result!!.user)
+                    moveMainPage(task.result?.user)
                 } else {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
